c666e616951ddd2f649312885b2e9224e6765fe7
Cameron <cky39v@umsystem.edu>
2021-03-23 13:09:59 -0500

    Added makefile and other files.
    Added and implemented the makefile. created some files to store the
    inventory of the character to be implemented with an item class that
    will contain the item type(weapon, object, food, valuable, etc.) the
    weight, the price, the stats, the name, and the description of each item. This will
    allow for the creation of custom items and the addition of any other
    currenty existing item.

---
+++ Makefile
@@ -0,0 +1,17 @@
+CXXFLAGS := -std=c++11 -Wall -Werror -ggdb -O0
+
+SOURCES := $(wildcard *.cpp)
+OBJS := $(SOURCES:%.cpp=%.o)
+
+
+TARGET := character
+
+.PHONY : all
+all : $(TARGET)
+
+$(TARGET) : $(OBJS)
+	 $(CXX) $(CXXFLAGS) $^ -o $@ 
+
+.PHONY : clean
+clean : 
+	rm -rf $(TARGET) $(OBJS)
---
+++ character.cpp
@@ -0,0 +1,25 @@
+#ifndef CHARACTER_H
+#define CHARACTER_H
+
+#include <string>
+#include <iostream>
+#include <vector>
+
+
+class base_char{
+	public: 
+
+	private:
+		std::string class_type;
+		std::string char_name;
+		std::string player_name;
+		std::string race;
+		int level;
+		std::vector<int> skills;
+		std::vector<int> ability_scores;
+
+
+
+
+};
+#endif 
