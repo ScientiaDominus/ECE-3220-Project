0078dedf202fb3c4396cbfa0b23caf56067f41f7
Cameron <cky39v@umsystem.edu>
2021-03-23 13:44:53 -0500

    Implmented simple return functions for characters
    Added some simple functions to return the different members of the
    character class. Every member is included. `

--- character.h
+++ character.h
@@ -9,6 +9,14 @@
 
 class base_char{
 	public: 
+        int get_level() const{return level;}
+        std::string get_class() const{return class_type;}
+        std::string get_name() const{return char_name;}
+        std::string get_player_name() const{return player_name;}
+        std::string get_race() const{return race;}
+        std::vector<int> get_skills() const{return skills;}
+        std::vector<int> get_ability_scores() const{return ability_scores;}
+        std::vector<item> get_inventory() const{return inventory;}
 
 	private:
 		std::string class_type;
