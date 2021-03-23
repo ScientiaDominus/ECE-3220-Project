105a6b58bf8c50b9896686d4eb8ed6bcd446cd10
Cameron <cky39v@umsystem.edu>
2021-03-23 13:30:53 -0500

    Started implementing the item class
    Started implementing the item class for use in the inventory. Also moved
    my character.cpp code to the character.h file where it should have been
    in the first place.

---
+++ character.h
@@ -0,0 +1,26 @@
+#ifndef CHARACTER_H
+#define CHARACTER_H
+
+#include <string>
+#include <iostream>
+#include <vector>
+
+
+class base_char{
+	public: 
+
+	private:
+		std::string class_type;
+		std::string char_name;
+		std::string player_name;
+		std::string race;
+		int level;
+		std::vector<int> skills;
+		std::vector<int> ability_scores;
+		std::vector<item> inventory;
+
+
+
+
+};
+#endif 
---
+++ item.h
@@ -0,0 +1,22 @@
+#ifndef ITEM_H
+#define ITEM_H
+#include <stdlib.h>
+#include <string>
+#include <iostream>
+
+class item{
+    public:
+
+    private:
+        std::string name;
+        std::string type;
+        int base_dmg;
+        int weight;
+        
+        
+        
+
+};
+
+
+#endif
