7ea65429f5f4a151988928ca720d09fd8439a1fe
Cameron <cky39v@umsystem.edu>
2021-04-11 01:37:28 -0500

    Added getters and setters for items class
    Everything so far has been implemented in item.h nothing has been
    implemented in the .cpp yet.

--- item.h
+++ item.h
@@ -6,13 +6,21 @@
 
 class item{
     public:
+        item(std::string name, std::string damage);
+
+        std::string getName() const{return name_;}
+        std::string getDamage() const{return damage_;}
+        
+        double getWeight_() const{return weight_;}
+        int getID_() const{return itemID_;}
+        int getPrice_() const{return price_;}
 
     private:
         std::string name_;
-        std::string type_;
-        std::string description_;
-        int base_dmg_;
-        int weight_;
+        //std::string description_; might implement this later not needed but it would be nice to have in the program
+        std::string damage_;
+        double weight_;
+        int itemID_;
         int price_;
 
         
