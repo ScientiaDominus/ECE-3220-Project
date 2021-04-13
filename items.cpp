2d5d47c2092c6e91e360230189152f8cdb276e5a
Cameron <cky39v@umsystem.edu>
2021-04-13 13:54:17 -0500

    Implemented the setters in item.cpp
    Added simple functionality to the functions to allow it to change the
    values stored within the variables of the parent class

--- items.cpp
+++ items.cpp
@@ -1,1 +1,31 @@
 #include "item.h"
+
+item::item(std::string name, std::string damage, double weight, int itemID, int price)
+{
+    name_ = name;
+    damage_ = damage;
+    weight_ = weight;
+    itemID_ = itemID;
+    price_ =  price;
+}
+
+void item::setName_(std::string name)
+{
+    name_ = name;
+}
+void item::setDamage_(std::string damage)
+{
+    damage_ = damage;
+}
+void item::setWeight_(double weight)
+{
+    weight_ = weight;
+}
+void item::setID_(int ID)
+{
+    itemID_ = ID;
+}
+void item::setPrice_(int price)
+{
+    price_ = price;
+}
