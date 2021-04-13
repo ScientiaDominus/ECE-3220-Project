81dc99c4a281347d5839e05841941289bb0eb066
Cameron <cky39v@umsystem.edu>
2021-04-13 13:44:41 -0500

    Added implementations for some setters
    item.h now has the containers for the setter functions as well as the
    implementation for the constructor for the program. This will simply set
    the values of the item object based on the parameters passed. Still need
    to figure out how to store the item vectors in a file and read them into
    an item object easily.

--- item.h
+++ item.h
@@ -6,7 +6,7 @@
 
 class item{
     public:
-        item(std::string name, std::string damage);
+        item(std::string name, std::string damage, double weight, int itemID, int price);
 
         std::string getName() const{return name_;}
         std::string getDamage() const{return damage_;}
@@ -15,6 +15,14 @@ class item{
         int getID_() const{return itemID_;}
         int getPrice_() const{return price_;}
 
+        void setName_(std::string name);
+        void setDamage_(std::string damage);
+        void setWeight_(double weight);
+        void setID_(int ID);
+        void setPrice_(int price);
+
+        
+        
     private:
         std::string name_;
         //std::string description_; might implement this later not needed but it would be nice to have in the program
