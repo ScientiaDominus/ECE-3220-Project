eae7091b5ff36be69b728798074a7d299a2dafbd
Cameron <cky39v@umsystem.edu>
2021-04-13 18:08:01 -0500

    Wrote the header file for the weapon class
    Not sure why we are doing inheritance for this now. The item parameters
    are different enough that I think having a weapon inherit from an item
    might be a little goofy. There are elements exclusive to the weapon that
    just make inherited classes more complicated than the class above it. At
    least how they are currently implemented.

--- armor.h
+++ armor.h
@@ -1,14 +1,3 @@
 #include "item.h"
 
-class weapon : public item
-{
-    public:
-        weapon(std::string name, std::string damage, double weight, int itemID, int price, std::string damageType, int range) : item(name, damage, weight, itemID, price)
-        {
-            std::cout << "weapon Constructor called" << std::endl;
-        }
 
-    private:
-        std::string damageType_; 
-        int range_;
-}
