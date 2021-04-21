aabb8f183540277bd26db686d5f1a611fa71bd81
Benjamin Chilson <benjameschilson@gmail.com>
2021-04-21 09:59:01 -0700

    added Singleton functionallity to the System class

---
+++ System.h
@@ -0,0 +1,39 @@
+#ifndef SYSTEM_H
+#define SYSTEM_H
+
+#include <stdlib.h>
+#include <string>
+#include <iostream>
+#include "Spell.h"
+#include "Item.h"
+#include "Character.h"
+
+class System{
+    private:
+        static System *instance; 
+        std::vector<Item> itemVector;
+        std::vector<Spell> spellVector;
+        std::vector<Character> characterVector;
+
+        System();
+    public:
+        void printItemList();
+        void printCharacter();
+        void printSpell();
+
+
+        void addItem(const Item &item);
+        void addSpell(const Spell &spell);
+        void addCharacter(const Character &character);
+        Character* getCharacter(std::string name);
+        Spell* getSpell(std::string name);
+        Item* getItem(std::string name);
+        Character* getCharacterByID(int charID);
+        Spell* getSpellByID(int spellID);
+        Item* getItemByID(int itemID);
+        static System *getInstance();
+
+
+};
+
+#endif
