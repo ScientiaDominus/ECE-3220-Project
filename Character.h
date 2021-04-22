da5ac26f6fe5eae34925548e5afcfe98c88e3ebc
Benjamin Chilson <benjameschilson@gmail.com>
2021-04-22 09:16:38 -0700

    added Race and CharacterClass enum conversion to string functions, added several toExportString functions, one for Character class and helper function for its member vectors

--- Character.h
+++ Character.h
@@ -16,17 +16,21 @@ class Character{
 		Character(std::string player_name, std::string character_name, CharacterClass character_class,
                     Race race, int level, AbilityScores ability_scores, std::vector<Item> item_inventory,
                     std::vector<Spell> spell_inventory, int gold_count);
-        std::string get_player_name() const;
+		std::string get_player_name() const;
 		std::string get_character_name() const;
 		CharacterClass get_character_class() const;
 		Race get_race() const;
 		int get_level() const;
-        AbilityScores get_ability_scores() const;
-        std::vector<Item> get_item_inventory() const;
+		AbilityScores get_ability_scores() const;
+		std::vector<Item> get_item_inventory() const;
 		std::vector<Spell> get_spell_inventory() const;
 		int get_gold_count() const;
 		std::string to_string() const;
-		std::string toExportString() const;
+		std::string toExportString();
+		std::string itemInventoryToExportString();
+		std::string spellInventoryToExportString();
+		std::string characterClassToString(CharacterClass characterClass);
+		std::string raceToString(Race race);
 
 	private:
 		std::string player_name;
