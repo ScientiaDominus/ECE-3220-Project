648eb0fdbb67768b0b6dc5a342f6b633069b1830
Benjamin Chilson <benjameschilson@gmail.com>
2021-04-30 15:30:11 -0700

    Menu now responds to prompts with and stores menuType

--- TemplateTest/System.h
+++ TemplateTest/System.h
@@ -6,41 +6,49 @@ enum MenuModeType {CHARACTER, ITEM, SPELL};
 class System{
     private:
         EntityList<Item> itemList;
-
+        //EntityList<Character> characterList;
+        //EntityList<Spell> spellList;
         MenuModeType menuMode;
+        std::string menuModeString;
+
     public:
         void StartMenu(){
             std::cout << "Main Menu. Pick and Option Below" << std::endl;
-            std::cout << "\t1) Item\n\tEverything Else) Exit" << std::endl;
+            std::cout << "\t1) Character\n\t2) Item\n\t3) Spell\n\t4) Exit" << std::endl;
             int response;
             std::cin >> response;
-            switch (response)
-            {
-            case 1:
-                EntityMenu<Item>(itemList);
-                break;
+            switch (response){
+                // case 1:
+                //     changeMenuMode(CHARACTER);
+                //     EntityMenu<Item>(characterList);
+                //     break;
+                case 2:
+                    changeMenuMode(ITEM);
+                    EntityMenu<Item>(itemList);
+                    break;
+                // case 3:
+                //     changeMenuMode(SPELL);
+                //     EntityMenu<Item>(spellList);
+                //     break;
             
-            default:
-            return;
-                break;
+                default:
+                    return;
             }
         }
 
         template<typename E>
         void EntityMenu(EntityList<E> list){
-            std::cout << "Welcome to the Entity Menu" << std::endl;
+            std::cout << "Welcome to the " << menuModeString << " Entity Menu" << std::endl;
             int response;
             std::cin >> response;
-            switch (response)
-            {
-            case 1:
-                std::cout << "Youve selected option 1!" << std::endl;
-                break;
-            case 2:
-                std::cout << "Youve selected option 2" << std::endl;
-            default:
-                return;
-                break;
+            switch (response){
+                case 1:
+                    std::cout << "Youve selected option 1!" << std::endl;
+                    break;
+                case 2:
+                    std::cout << "Youve selected option 2" << std::endl;
+                default:
+                    return;
             }
         }
         // template<typename E>
@@ -107,4 +115,22 @@ class System{
 
         //     std::cout << "View or Edit?" << std::endl;
         // }
+
+        void changeMenuMode(MenuModeType menuMode){
+            this->menuMode = menuMode;
+            switch (this->menuMode)
+            {
+                case CHARACTER:
+                    menuModeString = "Character";
+                    break;
+                case ITEM:
+                    menuModeString = "Item";
+                    break;
+                case SPELL:
+                    menuModeString = "Spell";
+                default:
+                    menuModeString = "";
+                    break;
+            }
+        }
 };
